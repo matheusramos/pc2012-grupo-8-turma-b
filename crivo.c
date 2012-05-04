c33276e5a949883afba4cd66e2eaf9b83937f0ab
vanessa.qm.1 <vanessa.qm.1@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-05-04 14:22:31 +0000

    Primeira versao do algoritmo do Crivo

--- crivo.c
+++ crivo.c
@@ -2,8 +2,128 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXLENGTH 200
 
-int main(int argc, char *argv[]){ 
+typedef struct
+{
+	int last_index;
+	int last_prime;
+	int max_len;
+	int *prime_list;
+}primos;
+
+void realoca (primos **p, int novo_tamanho)
+{
+	int i,*realocado;
+	realocado = realloc ((*p)->prime_list, novo_tamanho);
+	(*p)->prime_list = realocado;
+
+	for(i=0;i<= (*p)->last_index;++i)
+		printf("-Realocado%d\n-",realocado[i]);
+	
+}
+
+int crivo(primos *list, int valor)
+{ 
+
+	int i,j;
+	int *vetor,prime;
+	int raiz = sqrt(valor);
+
+	//temos que calcular mais primos
+	if(raiz > (list->last_prime))
+	{
+		int tamanho = raiz - (list->last_prime);
+
+		vetor = malloc(tamanho*sizeof(int));	
+		//preenche o vetor
+		for(i=0;i<tamanho;i++)
+			vetor[i]=(list->last_prime)+i+1;
+	
+		for(i=0;i<=(list->last_index);++i)
+		{
+			prime = list->prime_list[i];
+			for(j=0;j<tamanho;++j)
+			{
+				//se eh multiplo
+				if(vetor[j]%prime==0)
+					vetor[j]=-1;
+			
+			}
+			//jah percorreu toda a lista inicial de primos, eh hora de add mais algum primo
+			if(i==(list->last_index))
+			{
+				int posicao=0;
+				while(posicao <tamanho && vetor[posicao]==-1)
+					++posicao;
+				//achou um valor nao marcado
+				if(posicao<tamanho)
+				{
+					(list->last_index)++;
+					list->prime_list[list->last_index] = vetor[posicao];
+					list->last_prime = vetor[posicao];				
+					vetor[posicao]=-1;
+
+					//caso o vetor esteja no limite, ele eh realocado
+					//if(list->last_index == (list->max_len)-1)
+					//{
+						//int novo_tamanho = (list->max_len)*2;
+						//realoca(&list,novo_tamanho);
+						//list->max_len = novo_tamanho;
+	
+					//}
+				}	
+			
+			}
+		}	
+		
+	}	
+	
+	//depois que montou a tabela de primos vai dividindo... (nao tem como ter busca binaria)
+	int inf=0,pos,meio;
+	int achou=0;
+	pos  = list->last_index;
+	while(inf<=pos)
+	{
+		int meio=(inf+pos)/2;
+		if(list->prime_list[meio]==valor)
+		{
+			printf("Achouuu eh primo");
+			return 1;
+		}
+		else if (list->prime_list[meio]<valor)
+			inf = meio+1;
+		else
+			pos = meio-1;
+	}
+	
 	
 	return 1;
+}
+
+
+
+int main()
+{
+
+	primos *list;
+	list = malloc(sizeof(primos));
+	int i;
+
+	list->last_index=0;
+	list->last_prime = 2;
+	list->max_len = MAXLENGTH;
+	list->prime_list = malloc ((MAXLENGTH)*sizeof(int));
+	list->prime_list[0]=2;
+
+	crivo(list,997);	
+
+	for(i=0;i<=(list->last_index);++i)
+		printf("-%d-",(list->prime_list[i]));
+
+	printf("Tamanhoooo-%d-",(list->last_index));
+
+
+	return 1;
+
 }
