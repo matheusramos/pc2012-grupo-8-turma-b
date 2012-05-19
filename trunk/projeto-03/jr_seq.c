110d815b905c9ef3ae684c399122685dc882577d
matheusedsonramos@gmail.com <matheusedsonramos@gmail.com@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-05-19 13:11:59 +0000

    Leitura sequencial funcionando

--- trunk/projeto-03/jr_seq.c
+++ trunk/projeto-03/jr_seq.c
@@ -1,8 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * Função que aloca uma matriz M quadrada de ordem tamanho.
+ */
+void alocarMatrizQuadDoub(double ***M, int tamanho)
+{
+	register int i=0;
+	*M = (double **) malloc(tamanho*sizeof(double *));
+
+	for(i=0; i<tamanho; i++)
+		(*M)[i] = (double *) calloc(tamanho,sizeof(double));
+}
+
+/**
+ * Funcao que imprime uma matriz M quadrada de doubles de ordem tamanho.
+ */
+void imprimirMatrizQuadDoub(double **M, int tamanho)
+{
+	register int i=0,j=0;
+	
+	for(i=0; i<tamanho; i++)
+	{
+		for(j=0; j<tamanho; j++)
+			printf("%lf ",M[i][j]);
+		printf("\n");
+	}
+}
+
 int main(int argc, char **argv)
 {
+	double **MA;
+	int j_order=0; //Ordem da matriz
+	int j_row_test=0; //Linha(Fila) para ser avaliada
+	double j_error=0; //Erro permitido
+	int j_ite_max=0; //Numero máximo de iterações
+
+	register int i=0,j=0; //int para iteração
+
+	scanf("%d",&j_order);
+
+	alocarMatrizQuadDoub(&MA,j_order); //TODO:Paralelizar alocacao enquanto ocorre o resto da leitura
+	
+	scanf("%d",&j_row_test);
+	scanf("%lf",&j_error);
+	scanf("%d",&j_ite_max);
+	
+	/*Lê a matriz A*/
+	for(i=0; i<j_order; i++)
+		for(j=0; j<j_order; j++)
+			scanf("%lf",&MA[i][j]);
+
+	imprimirMatrizQuad(MA,j_order);	
+	
 
 	return EXIT_SUCCESS;
 }
