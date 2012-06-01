3d013e7a202ea692a229e9c83e54852b487bbdfc
matheusedsonramos@gmail.com <matheusedsonramos@gmail.com@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-06-01 04:29:43 +0000

    Paralelo execucao cluster

--- trunk/projeto-03/par/par_executa_cluster.sh
+++ trunk/projeto-03/par/par_executa_cluster.sh
@@ -1,17 +1,16 @@
-for i in `ls ../entradas/peq/`
+for i in `ls peq/jr_par*.c`
 do
 	for (( j = 0 ; j < 5 ; j++ ))
 	do
-		#(time mpirun -np 4 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno jr_par) 2>>par_$i.txt
-		(time mpirun -np 4 jr_par) 2>>par_$i.txt
+		(time mpirun -np 4 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
 	done
 done
 
-for i in `ls ../entradas/grd/`
+for i in `ls grd/jr_par*.c`
 do
 	for (( j = 0 ; j < 3 ; j++ ))
 	do
-		(time ./jr_seq < ../entradas/grd/$i) 2>>seq_$i.txt
+		(time mpirun -np 4 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
 	done
 done
 
