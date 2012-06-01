1d533d1fe7bed624022562c3d27cc956f43db7fc
matheusedsonramos <matheusedsonramos@gmail.com>
2012-06-01 06:05:06 +0000

    np 13

---
+++ projeto-03/par/grd/pargrd_executa_cluster.sh
@@ -0,0 +1,8 @@
+for i in `ls jr_par*`
+do
+	for (( j = 0 ; j < 3 ; j++ ))
+	do
+		(time mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
+	done
+done
+
