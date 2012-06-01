20e4ab6f8b922ae8f6a2d54e777072e848b41521
matheusedsonramos <matheusedsonramos@gmail.com>
2012-06-01 05:54:55 +0000

    np 13

--- projeto-03/par/par_executa_cluster.sh
+++ projeto-03/par/par_executa_cluster.sh
@@ -2,7 +2,7 @@ for i in `ls peq/jr_par*.c`
 do
 	for (( j = 0 ; j < 5 ; j++ ))
 	do
-		(time mpirun -np 4 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
+		(time mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
 	done
 done
 
@@ -10,7 +10,7 @@ for i in `ls grd/jr_par*.c`
 do
 	for (( j = 0 ; j < 3 ; j++ ))
 	do
-		(time mpirun -np 4 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
+		(time mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
 	done
 done
 
