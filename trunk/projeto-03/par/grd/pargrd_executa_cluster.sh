06f0e755c4cfe8dff1b5463334e4a451f08885be
matheusedsonramos@gmail.com <matheusedsonramos@gmail.com@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-06-03 20:34:48 +0000

    Teste para o cluster

--- trunk/projeto-03/par/grd/pargrd_executa_cluster.sh
+++ trunk/projeto-03/par/grd/pargrd_executa_cluster.sh
@@ -1,6 +1,6 @@
 for i in `ls jr_par*`
 do
-	for (( j = 0 ; j < 3 ; j++ ))
+	for (( j = 0 ; j < 5 ; j++ ))
 	do
 		(time mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno $i) 2>>par_$i.txt
 	done
