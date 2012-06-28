6d7d695b3db1eedd7fbd31c03e63945382f9748b
matheusedsonramos@gmail.com <matheusedsonramos@gmail.com@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-06-28 06:31:32 +0000

    Paralelos para execução no cluster

---
+++ trunk/projeto-final/par2.sh
@@ -0,0 +1,5 @@
+#for (( j = 0 ; j < 5 ; j++ ))
+#do
+	mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par2 >> par2.txt
+#done
+
