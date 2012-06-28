5cb3cbc8b3acb2a8d17d221652b31b3cf5ca984c
fabio.sikansi <fabio.sikansi@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-06-28 09:47:28 +0000

    Update scripts de teste paralelo

--- projeto-final/timetest_par.sh
+++ projeto-final/timetest_par.sh
@@ -1,56 +1,56 @@
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste par.c" 2>>teste2_par.txt
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 4 hosts, 4 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste par.c" >> teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 4 hosts, 4 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 4 --host wolverine,magneto,hulk,colossus macacos_par 2>> teste2_par.txt
+	time mpirun -np 4 --host wolverine,magneto,hulk,colossus macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 4 hosts, 8 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 4 hosts, 8 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 8 --host wolverine,magneto,hulk,colossus macacos_par 2>> teste2_par.txt
+	time mpirun -np 8 --host wolverine,magneto,hulk,colossus macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 4 hosts, 16 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 4 hosts, 16 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 16 --host wolverine,magneto,hulk,colossus macacos_par 2>> teste2_par.txt
+	time mpirun -np 16 --host wolverine,magneto,hulk,colossus macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 8 hosts, 8 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 8 hosts, 8 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 8 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira macacos_par 2>> teste2_teste2_par.txt
+	time mpirun -np 8 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira macacos_par 2>>  teste2_teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 8 hosts, 16 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 8 hosts, 16 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 16 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira macacos_par 2>> teste2_par.txt
+	time mpirun -np 16 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 8 hosts, 32 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 8 hosts, 32 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 32 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira macacos_par 2>> teste2_par.txt
+	time mpirun -np 32 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 13 hosts, 13 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 13 hosts, 13 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par 2>> teste2_par.txt
+	time mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 13 hosts, 26 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 13 hosts, 26 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 26 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par 2>> teste2_par.txt
+	time mpirun -np 26 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par 2>>  teste2_par.txt
 #done
-echo "------------------------------------------------" 2>>teste2_par.txt
-echo "Teste para 13 hosts, 52 processos" 2>>teste2_par.txt
+echo "------------------------------------------------" >> teste2_par.txt
+echo "Teste para 13 hosts, 52 processos" >> teste2_par.txt
 #for (( j = 0 ; j < 6; j++ ))
 #do
-	time mpirun -np 52 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par 2>> teste2_par.txt
+	time mpirun -np 52 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par 2>>  teste2_par.txt
 #done
