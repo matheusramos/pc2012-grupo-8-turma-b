d1703603429e7c1737c28d432a7e5b8d56117921
fabio.sikansi <fabio.sikansi@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-06-28 02:22:15 +0000

    Adicionado script para realizar varios testes sequencial

---
+++ projeto-final/Sequencial/testatodos.sh
@@ -0,0 +1,64 @@
+echo "------------------------------------------------" >>result.txt
+echo "Teste para 10% das palavras" >>result.txt
+echo "Teste 1" >>result.txt
+(time ./macacos10p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 2" >>result.txt
+(time ./macacos10p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 3" >>result.txt
+(time ./macacos10p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 4" >>result.txt
+(time ./macacos10p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 5" >>result.txt
+(time ./macacos10p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste para 20% das palavras" >>result.txt
+echo "Teste 1" >>result.txt
+(time ./macacos20p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 2" >>result.txt
+(time ./macacos20p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 3" >>result.txt
+(time ./macacos20p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 4" >>result.txt
+(time ./macacos20p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 5" >>result.txt
+(time ./macacos20p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste para 50% das palavras" >>result.txt
+echo "Teste 1" >>result.txt
+(time ./macacos50p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 2" >>result.txt
+(time ./macacos50p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 3" >>result.txt
+(time ./macacos50p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 4" >>result.txt
+(time ./macacos50p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 5" >>result.txt
+(time ./macacos50p) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste para 100% das palavras" >>result.txt
+echo "Teste 1" >>result.txt
+(time ./macacos) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 2" >>result.txt
+(time ./macacos) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 3" >>result.txt
+(time ./macacos) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 4" >>result.txt
+(time ./macacos) 2>>result.txt
+echo "------------------------------------------------" >>result.txt
+echo "Teste 5" >>result.txt
+(time ./macacos) 2>>result.txt
