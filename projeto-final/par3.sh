ebec6f3854f9b4b00bc74a3fcdbc1d6ba72f1a6a
fabio.sikansi <fabio.sikansi@4d2663ff-bc5e-e71c-f166-d0f428d26e14>
2012-06-30 14:33:58 +0000

    Remoção de arquivos adicionais e inclusão do README

--- projeto-final/par3.sh
+++
@@ -1,5 +0,0 @@
-#for (( j = 0 ; j < 5 ; j++ ))
-#do
-	mpirun -np 13 --host wolverine,magneto,hulk,colossus,tempestade,demolidor,venon,vampira,gambit,elektra,anjo,justiceiro,noturno macacos_par3 >> par3.txt
-#done
-
